3152b2f6c0c5caf1b709cacb34a171fcefff0ee1
RH-Jewel <riponhossain876@gmail.com>
2024-11-16 01:10:26 +0600

    add sections

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,26 @@
 (function ($) {
     "use strict"
 
+    // var swiper = new Swiper(".hero-bg-slider", {
+    //     slidesPerView: 1,
+    //     spaceBetween: 0,
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //     },
+    // });
+
+    var swiper = new Swiper(".feature-slider", {
+        slidesPerView: 3,
+        loop: true,
+        watchSlidesProgress: true,
+        navigation: {
+            nextEl: ".slider-next",
+            prevEl: ".slider-prev",
+        },
+        autoplay: {
+            delay: 5000,
+        },
+    });
 
 
 }(jQuery));
